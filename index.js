243152021522b981a2395fc87bae6ee746707dfe
Yao Xian Phang <80228647+yaoxianphang@users.noreply.github.com>
2024-06-11 22:34:21 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
 
+app.use(express.static("dist"));
 app.use(cors());
 app.use(express.json());
 
@@ -101,7 +102,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
