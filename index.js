4e578628134fbeef8ac66d3d51bfe14800eefe7a
Yao Xian Phang <80228647+yaoxianphang@users.noreply.github.com>
2024-06-11 17:39:07 +1000

    Completed exercises 3.7 and 3.8

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+
+// Configure morgan
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
