b1abf95eaa87d599a93b8d9dc68c90afa53d72f1
Yao Xian Phang <80228647+yaoxianphang@users.noreply.github.com>
2024-06-11 22:30:46 +1000

    Merged with front end

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 // Configure morgan
